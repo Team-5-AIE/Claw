29d0c44f6e174ae2b7b34b4ea82ab7a3d0ce2326
Emma Stitt <emmcooke@optusnet.com.au>
2024-08-26 17:37:21 +1000

    Attempted to add scene visualisation for the scene transition nodes

--- Project/Scripts/RoomTransition.gd
+++ Project/Scripts/RoomTransition.gd
@@ -16,14 +16,10 @@ var player : SWPlatformerCharacter
 var playerCamera : Camera2D
 
 # Private variables
-var _exitDirection : Vector2 = Vector2(1, 0)
+@onready var _resourcePreviewer : EditorResourcePreview = EditorInterface.get_resource_previewer()
 
-var _lineLength : float = 30
-var _lineColour : Color = Color(1, 0, 0, 0.5)
-var _lineWidth : float = 3
-
-var _headLength : float = 20
-var _headAngle : float = 40
+var _previewCanvasItem : RID
+var _previewCanvasTexture : RID
 
 # ---Functions---
 # Init
@@ -42,13 +38,8 @@ func _process(_delta):
 
 func _draw():
 	if Engine.is_editor_hint():
-		var exitLine : Vector2 = _exitDirection * _lineLength
-		draw_line(Vector2.ZERO, exitLine, _lineColour, _lineWidth)
-		var arrowHead : Vector2 = -_exitDirection * _headLength
-		draw_line(exitLine, arrowHead.rotated(_headAngle),
-				  _lineColour, _lineWidth)
-		draw_line(exitLine, arrowHead.rotated(-_headAngle),
-				  _lineColour, _lineWidth)
+		DrawArrow(Vector2.RIGHT, 30, 3, Color(1, 0, 0, 0.5))
+		_resourcePreviewer.queue_resource_preview(nextScene.resource_path, self, "DrawRoomPreview", null)
 
 # RoomTransition signals
 func _on_body_entered(body_):
@@ -61,3 +52,28 @@ func _on_body_entered(body_):
 			roomTransition.set_process_mode(Node.PROCESS_MODE_DISABLED)
 		
 		playerEnteredRoom.emit(body_)
+
+# EditorResourcePreview reciever function
+func DrawRoomPreview(path : String, preview : Texture2D, thumbnail_preview : Texture2D,
+					 userdata : Variant) -> Variant:
+	draw_texture(preview, Vector2.ZERO)
+	#_previewCanvasItem = RenderingServer.canvas_item_create()
+	#_previewCanvasTexture = RenderingServer.canvas_texture_create()
+	#
+	#preview.draw_rect(_previewCanvasItem, preview.get_size())
+	
+	return userdata
+
+# Custom functions
+func DrawArrow(direction_ : Vector2, length_ : float, lineWidth_ : float, colour_ : Color):
+	# Arrow constants
+	var headLength : float = 20
+	var headAngle : float = 40
+	
+	var exitLine : Vector2 = direction_ * length_
+	draw_line(Vector2.ZERO, exitLine, colour_, lineWidth_)
+	var arrowHead : Vector2 = -direction_ * length_
+	draw_line(exitLine, arrowHead.rotated(headAngle),
+			  colour_, lineWidth_)
+	draw_line(exitLine, arrowHead.rotated(-headAngle),
+			  colour_, lineWidth_)
