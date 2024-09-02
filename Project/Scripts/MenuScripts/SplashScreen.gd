67ca5bed18989fc418c2ae2de46177dbcf2cffce
missing <166961348+M1ssing-Pixel@users.noreply.github.com>
2024-09-02 21:19:08 +1000

    More spleash Screen shenanigans

--- Project/Scripts/MenuScripts/SplashScreen.gd
+++ Project/Scripts/MenuScripts/SplashScreen.gd
@@ -8,9 +8,8 @@ func _ready():
 	tween.tween_property(getrect, "color", Color8(218, 134, 62, 255), 1.3)
 	tween.tween_property(getSprite, "modulate", Color.WHITE, 1).set_ease(Tween.EASE_IN)
 	
-	var fade_tween = create_tween().set_loops()
-	fade_tween.tween_property($"../Control/CanvasLayer/Label","modulate", Color.TRANSPARENT, 1).set_delay(0.4).set_ease(Tween.EASE_IN)
-	fade_tween.tween_property($"../Control/CanvasLayer/Label","modulate", Color(1,1,1,0.5), 1).set_delay(0.4).set_ease(Tween.EASE_OUT)
+	var fade_tween = get_tree().create_tween()
+	fade_tween.tween_property($"../Control/CanvasLayer/Label","modulate", Color.TRANSPARENT, 0.5).set_delay(1).set_ease(Tween.EASE_OUT)
 
 func _on_animation_start_timeout() -> void:
 	getSprite.play()
