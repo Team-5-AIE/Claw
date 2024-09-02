3b9f9f13dbdc9984a1bb4cb6b9d51585ba465df6
missing <166961348+M1ssing-Pixel@users.noreply.github.com>
2024-09-02 19:11:47 +1000

    Added the ever covetted "skip" button

--- Project/Scripts/MenuScripts/SplashScreen.gd
+++ Project/Scripts/MenuScripts/SplashScreen.gd
@@ -2,10 +2,15 @@ extends Node
 @onready var getSprite: AnimatedSprite2D = $"."
 @onready var getrect: ColorRect = $"../Control/CanvasLayer/ColorRect"
 
+
 func _ready():
 	var tween = get_tree().create_tween()
 	tween.tween_property(getrect, "color", Color8(218, 134, 62, 255), 1.3)
 	tween.tween_property(getSprite, "modulate", Color.WHITE, 1).set_ease(Tween.EASE_IN)
+	
+	var fade_tween = create_tween().set_loops()
+	fade_tween.tween_property($"../Control/CanvasLayer/Label","modulate", Color.TRANSPARENT, 1).set_delay(0.4).set_ease(Tween.EASE_IN)
+	fade_tween.tween_property($"../Control/CanvasLayer/Label","modulate", Color(1,1,1,0.5), 1).set_delay(0.4).set_ease(Tween.EASE_OUT)
 
 func _on_animation_start_timeout() -> void:
 	getSprite.play()
@@ -19,7 +24,13 @@ func _on_animation_looped() -> void:
 	$"../Change Scenes".start()
 	
 	var tween = get_tree().create_tween()
-	tween.tween_property(getSprite, "modulate", Color(1,1,1,0),1.5).set_delay(1).set_ease(Tween.EASE_OUT)
+	tween.tween_property(getSprite, "modulate", Color.TRANSPARENT,1.5).set_delay(1).set_ease(Tween.EASE_OUT)
 	
 	var tween2 = get_tree().create_tween()
 	tween2.tween_property(getrect, "color", Color8(9,10,20,255), 1.8).set_delay(1).set_ease(Tween.EASE_OUT)
+	
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey:
+		if event.keycode == KEY_SPACE && event.is_pressed():
+			print("Skipped")
+			get_tree().change_scene_to_file("res://Chapters/Chap_Prototype/Proto_Level_01.tscn")
