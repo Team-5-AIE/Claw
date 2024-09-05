b512107b28924159064b14b7e83dc1154d619082
TraveenW <traveenw@gmail.com>
2024-09-05 23:46:50 +1000

    Overhauled control scheme to allow for more testing with controller binds

--- Project/Scripts/DebugScripts/crosshair.gd
+++ Project/Scripts/DebugScripts/crosshair.gd
@@ -2,33 +2,33 @@ extends Node2D
 
 enum ControlConfig {HOLD, TOGGLE}
 
+@export var crosshairs_enabled: bool = true
 @export var control_config: ControlConfig = ControlConfig.TOGGLE
+@export var is_mouse_used: bool = false
 @export var crosshair_reach: float = 60
-@export var aim_crosshair_delay_time: float = 0.1
+@export var aim_crosshair_delay: float = 0.1
+@export var aim_duration: float = 0.1
 
 var aim_direction: Vector2
-var aim_time: float = 0.1
-var aim_timer: float = 0
-var spear_used: bool = false
-var is_mouse_used: bool = false
 
-@onready var player = $"../.." as SWPlatformerCharacter
+@onready var player = $".." as SWPlatformerCharacter
 @onready var aim_visual = $AimPoint as Sprite2D
 @onready var throw_visual = $AimCrosshair as Sprite2D
 @onready var swing_visual = $SwingPoint as Sprite2D
 @onready var launch_visual = $SwingCrosshair as Sprite2D
 
 func _set_crosshair_visibility():
-	if player.finite_state_machine.state == player.state_spear:
-		aim_visual.visible = false
-		throw_visual.visible = false
-		launch_visual.visible = true
-		swing_visual.visible = true
-	elif control_config == ControlConfig.TOGGLE || is_mouse_used:
-		aim_visual.visible = true
-		throw_visual.visible = true
-		launch_visual.visible = false
-		swing_visual.visible = false
+	if crosshairs_enabled:
+		if player.finite_state_machine.state == player.state_spear:
+			aim_visual.visible = false
+			throw_visual.visible = false
+			launch_visual.visible = true
+			swing_visual.visible = true
+		else:
+			aim_visual.visible = true
+			throw_visual.visible = true
+			launch_visual.visible = false
+			swing_visual.visible = false
 	else:
 		aim_visual.visible = false
 		throw_visual.visible = false
@@ -43,7 +43,7 @@ func _set_crosshair_position(delta: float):
 	else:
 		aim_visual.position = Vector2(player.last_input_direction.x,-1).normalized() * crosshair_reach
 	
-	var spear_direction = (aim_visual.position - player.velocity * aim_crosshair_delay_time).normalized()
+	var spear_direction = (aim_visual.position - player.velocity * aim_crosshair_delay).normalized()
 	throw_visual.position = spear_direction * crosshair_reach
 	
 	if player.finite_state_machine.state == player.state_spear:
@@ -60,28 +60,27 @@ func _set_crosshair_position(delta: float):
 			#ControlConfig.HOLD:
 				#$"../../../CanvasLayer/ExitLabel".text = "HOLD (Press P to switch configs)\nLeft Stick - Move\nRight Stick - Shoot/pull Spear\nA/LB - Jump\nX - Slide"
 
-func _held_joystick_config(delta: float):
-	if aim_direction != Vector2.ZERO || Input.is_action_pressed("Spear"):
-		if player.finite_state_machine.state == player.state_spear:
-			aim_timer = 0
-		else:
-			aim_timer += delta
-			if aim_timer >= aim_time && not spear_used && player.spearCooldownTimer.time_left <= 0:
-					aim_timer = 0
-					player.finite_state_machine.ChangeState(player.state_spear)
-					spear_used = true
-	else:
-		aim_timer = 0
-		spear_used = false
-		
-		if player.finite_state_machine.state == player.state_spear:
-			var spearToPlayer = player.spear_marker.global_position - player.state_spear.spearInstance.global_position
-			
-			player.state_spear.audio_stream_player.stream = player.state_spear.PULLJUMP
-			player.state_spear.audio_stream_player.play()
-			player.state_spear.spearInstance.pullReleased = true
-			player.velocity *= (1.0 - player.state_spear.pullJumpStopFraction)
-			player.velocity += -spearToPlayer.normalized() * player.state_spear.pullJumpStrength
+func _controller_config(delta: float):
+	var controller_spear_use: InputEvent = InputEventAction.new()
+	match (control_config):
+		ControlConfig.TOGGLE:
+			if Input.is_action_just_pressed("SpearController"):
+				if player.finite_state_machine.state == player.state_spear:
+					print("TOGGLE: Let go of spear")
+					controller_spear_use.action = "LetGoOfSpear"
+				else:
+					print("TOGGLE: Used spear")
+					controller_spear_use.action = "Spear"
+		ControlConfig.HOLD:
+			if Input.is_action_just_pressed("SpearController") && player.finite_state_machine.state != player.state_spear:
+				print("HOLD: Used spear")
+				controller_spear_use.action = "Spear"
+			elif Input.is_action_just_released("SpearController") && player.finite_state_machine.state == player.state_spear:
+				print("HOLD: Let go of spear")
+				controller_spear_use.action = "LetGoOfSpear"
+	
+	controller_spear_use.pressed = true
+	Input.parse_input_event(controller_spear_use)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -92,26 +91,16 @@ func _process(delta: float) -> void:
 	_set_crosshair_position(delta)
 
 	
-	if control_config == ControlConfig.HOLD:
-		_held_joystick_config(delta)
+	if not is_mouse_used:
+		player.lockspear45direction = (aim_direction == Vector2.ZERO)
+		
+		_controller_config(delta)
 	
-	if control_config == ControlConfig.TOGGLE:
-		if aim_direction == Vector2.ZERO && not is_mouse_used:
-			player.lockspear45direction = true
-		else:
-			player.lockspear45direction = false
-	
-	#if Input.is_action_just_pressed("Debug Toggle"):
-		#match (control_config):
-			#ControlConfig.HOLD:
-				#control_config = ControlConfig.TOGGLE
-			#ControlConfig.TOGGLE:
-				#control_config = ControlConfig.HOLD
-		#_display_label()
-
-func _input(event: InputEvent) -> void:
-	if event is InputEventMouseMotion:
-		if aim_direction == Vector2.ZERO:
-			is_mouse_used = true
-		else:
-			is_mouse_used = false
+	if Input.is_action_just_pressed("Debug Toggle"):
+		match (control_config):
+			ControlConfig.HOLD:
+				print("Toggle Controls")
+				control_config = ControlConfig.TOGGLE
+			ControlConfig.TOGGLE:
+				print("Hold Controls")
+				control_config = ControlConfig.HOLD
