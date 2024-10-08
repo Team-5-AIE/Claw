f008ee33fe92b38d512469c3af0b2e639946a17c
TraveenW <traveenw@gmail.com>
2024-10-08 12:24:39 +1100

    refined functionality showcase of doors and keys

--- Project/TraveenPrototypes/Prototype_Features/Level Features/DoorLock/key.gd
+++ Project/TraveenPrototypes/Prototype_Features/Level Features/DoorLock/key.gd
@@ -25,10 +25,12 @@ func _ready() -> void:
 	animation_player.play("Float")
 
 func delete_key(delete: bool) -> void:
+	var initialGlobalPosition = global_position
 	# Move hole to door parent
 	keyImpression.visible = true
-	keyImpression.call_deferred("reparent", doorParent)
-	keyImpression.global_position = global_position
+	remove_child(keyImpression)
+	doorParent.add_child(keyImpression)
+	keyImpression.global_position = initialGlobalPosition
 	key_collected.emit(keyID)
 	queue_free()
 
@@ -44,11 +46,11 @@ func _physics_process(_delta):
 func _process(_delta)-> void:
 	if player != null:
 		targetPosition = player.bloomieMarker2D.global_position
-		global_position.x = lerp(global_position.x, targetPosition.x, 0.1)
-		global_position.y = lerp(global_position.y, targetPosition.y, 0.1)
+		global_position.x = lerp(global_position.x, targetPosition.x, 0.1 * (keyID + 1))
+		global_position.y = lerp(global_position.y, targetPosition.y, 0.1 * (keyID + 1))
 	else:
-		position.x = lerp(position.x, initialPosition.x, 0.1)
-		position.y = lerp(position.y, initialPosition.y, 0.1)
+		position.x = lerp(position.x, initialPosition.x, 0.1 * (keyID + 1))
+		position.y = lerp(position.y, initialPosition.y, 0.1 * (keyID + 1))
 	
 	if collected:
 		if !audio_stream_player.playing:
@@ -66,6 +68,7 @@ func _on_destroy_timer_timeout():
 	collected = true
 
 func _on_restart_player() -> void:
+	var initialGlobalPosition = global_position
 	if player != null:
 		follow = false
 		
@@ -80,24 +83,31 @@ func _on_restart_player() -> void:
 			queue_free()
 		else:
 			doorParent.add_child(self)
-			position = initialPosition
+			global_position = initialGlobalPosition
 			doorParent.keys.append
 			
-			keyImpression.call_deferred("reparent", self)
+			doorParent.remove_child(keyImpression)
+			add_child(keyImpression)
+			keyImpression.global_position = initialGlobalPosition
 			keyImpression.visible = false
 			
 			player = null
 
 func _on_area_2d_body_entered(body) -> void:
+	var initialGlobalPosition = global_position
 	if player == null and body.name == "Player":
 		player = body
 		top_level = true
 		
 		# Move hole to door parent
 		keyImpression.visible = true
-		keyImpression.call_deferred("reparent", doorParent)
+		remove_child(keyImpression)
+		doorParent.add_child(keyImpression)
+		keyImpression.global_position = initialGlobalPosition
 		
-		call_deferred("reparent", player)
+		doorParent.remove_child(self)
+		player.add_child(self)
+		global_position = initialGlobalPosition
 		
 		player.restartPlayer.connect(_on_restart_player)
 		
