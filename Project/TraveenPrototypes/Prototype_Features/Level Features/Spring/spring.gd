23fe71317d0c612552876f617241db9abfa559e6
TraveenW <traveenw@gmail.com>
2024-10-08 15:53:29 +1100

    spring now can now modify its angle of launch on top of the rotation

--- Project/TraveenPrototypes/Prototype_Features/Level Features/Spring/spring.gd
+++ Project/TraveenPrototypes/Prototype_Features/Level Features/Spring/spring.gd
@@ -1,11 +1,17 @@
 extends Area2D
 
 @export var springStrength: float = 350
-@export_range(0, 1) var springStopFraction: float = 0.8
+@export_range(-90, 90) var launchAngle: float = 0
+@export_range(0, 1) var launchStopFraction: float = 0.8
+
+var launchDirection: Vector2
+
+func _ready() -> void:
+	launchDirection = Vector2.UP.rotated(self.rotation + deg_to_rad(launchAngle))
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is SWPlatformerCharacter:
-		body.velocity *= 1 - springStopFraction
-		body.velocity += Vector2.UP.rotated(self.rotation) * springStrength
+		body.velocity *= 1 - launchStopFraction
+		body.velocity += launchDirection * springStrength
 		if body.finite_state_machine.state != body.state_spear:
 			body.finite_state_machine.ChangeState(body.state_fall)
