fc8f1c7a120db1ecb24d1e856d912eb90e20948b
TraveenW <traveenw@gmail.com>
2024-11-19 13:04:36 +1100

    Simplified time tracker so that it can be pausable

--- Project/Scripts/MenuScripts/time_tracker.gd
+++ Project/Scripts/MenuScripts/time_tracker.gd
@@ -1,48 +1,55 @@
 extends Control
 class_name TimeTracker
 @onready var label: Label = $Label
-@onready var seconds_timer: Timer = $SecondsTimer
-@onready var minutes_timer: Timer = $MinutesTimer
 
+var currentTime: float = 0
+var currentTimeString : String = "" #This can be saved into the Json files emma is working on for display.
+
+var hours : int = 0
 var minutes : int = 0
 var seconds : int = 0
-var currentTimeString : String = "" #This can be saved into the Json files emma is working on for display.
 var wholeTime : int = 0 #Used for easier sorting.
+
 func _ready() -> void:
 	visible = false
 	UpdateText()
 
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
+	currentTime += delta
+	wholeTime = floor(currentTime)
+	seconds = floor(currentTime) % 60
+	minutes = floor(currentTime/60)
+	hours = floor(currentTime/3600)
 	UpdateText()
 
 func UpdateText() -> void:
-	var secondsStr = str(seconds)
-	if seconds < 10:
-		secondsStr = "0" + str(seconds)
-	currentTimeString = str(minutes,":",secondsStr)
+	var secondsStr = "0" if seconds < 10 else ""
+	secondsStr += str(seconds)
+	
+	var minutesStr = "0" if minutes < 10 else ""
+	minutesStr += str(minutes % 60)
+	
+	var hoursStr = ""
+	if hours >= 10:
+		hoursStr = str(hours)
+		currentTimeString = str(hours, ":", minutesStr,":",secondsStr)
+	elif hours >= 1:
+		hoursStr = "0" + str(hours)
+		currentTimeString = str(hours, ":", minutesStr,":",secondsStr)
+	else:
+		currentTimeString = str(minutesStr,":",secondsStr)
 	label.text = currentTimeString
 
 func StartTimer():
-	seconds_timer.start()
-	minutes_timer.start()
 	currentTimeString = ""
+	currentTime = 0
+	wholeTime = 0
 	seconds = 0
 	minutes = 0
-	wholeTime = 0
+	hours = 0
 	visible = true
 
-func _on_seconds_timer_timeout() -> void:
-	seconds += 1
-	wholeTime += 1
-	seconds_timer.start()
-
-func _on_minutes_timer_timeout() -> void:
-	minutes += 1
-	seconds = 0
-	minutes_timer.start()
-
 func StopTimerAddToLastScore() -> void: #Call when level is finished.
-	seconds_timer.stop()
-	minutes_timer.stop()
+	visible = false
 	#NOTE: change the "Name" to be whatever the player types.
-	Global.lastScore = ["Name",currentTimeString,wholeTime]
+	Global.lastScore = ["Name",str(minutes,":",seconds),wholeTime]
